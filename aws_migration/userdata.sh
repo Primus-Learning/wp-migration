3266908aeb2f031027e3d4afbe542740d69992f7
babah1139 <103774473+babah1139@users.noreply.github.com>
2024-10-17 11:12:56 +0200

    updating tf code (#1)

---
+++ aws_migration/userdata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd mysql php php-mysql
+systemctl start httpd
+systemctl enable httpd
+cd /var/www/html
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+mv wordpress/* .
+chown -R apache:apache /var/www/html
+systemctl restart httpd
