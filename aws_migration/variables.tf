76f94b655145f84e4f97af2aecbe6a580dc43346
babah1139 <103774473+babah1139@users.noreply.github.com>
2024-10-21 08:02:03 +0200

    Tf update2 (#3)
    * updating tf code
    
    * update ami with wp gold image

--- aws_migration/variables.tf
+++ aws_migration/variables.tf
@@ -1,8 +1,8 @@
 variable "ami" {
   description = "The AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-00f251754ac5da7f0"
 
+  default     = "ami-0025c36cba2a6c3b9" # "ami-00f251754ac5da7f0" 
 
 }
 
@@ -16,5 +16,4 @@ variable "availability_zone" {
   description = "The availability zone for the subnet"
   type        = string
   default     = "us-east-1a"
-
 }
